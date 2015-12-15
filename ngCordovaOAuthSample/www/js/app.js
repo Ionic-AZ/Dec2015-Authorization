ae76a327bb785a07f543ca03498b4765f8627ef8
Justin James <digitaldrummerj@gmail.com>
2015-12-15 12:33:17 -0700

    added ngCordovaOAuth Sample

---
+++ ngCordovaOAuthSample/www/js/app.js
@@ -0,0 +1,68 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'ngCordova', 'ngCordovaOauth'])
+
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
+  .controller('AppCtrl', function ($scope, $cordovaOauth, $ionicPlatform, $http) {
+      // $http.get('https://api.github.com/user?access_token=2b6fdc18a467b1827d7a52ce9b81e07cda9f9602')
+      //         .then(function (authdata) {
+      //           $scope.github = authdata;
+      // });
+      //         
+      
+      function QueryStringToJSON(txt) {            
+    var pairs = txt.split('&');
+    
+    var result = {};
+    pairs.forEach(function(pair) {
+        pair = pair.split('=');
+        result[pair[0]] = decodeURIComponent(pair[1] || '');
+    });
+
+    return JSON.parse(JSON.stringify(result));
+    }
+
+    $scope.login = function (authMethod) {
+      $ionicPlatform.ready(function () {
+        if (authMethod === "google") {
+
+        } else if (authMethod === "github") {
+          //https://developer.github.com/v3/oauth/#scopes
+          $cordovaOauth.github("c38f92f08656340e0275", 'bd91fca1f33327be3a74017637051a3c4a893610', ["user:email"]).then(function (result) {
+            $scope.githubraw = result;
+            $scope.githubjson = QueryStringToJSON(result);
+            console.log('githubjson', $scope.githubjson);            
+            console.log('githubraw', $scope.githubraw);
+            
+            var access_token = $scope.githubjson.access_token;
+            console.log("access token", access_token);
+            
+            $http.get('https://api.github.com/user?access_token=' + access_token)
+              .then(function (authdata) {
+                $scope.github = authdata;
+              });
+          }, function (error) {
+            console.log(error);
+          });
+        } else if (authMethod === "facebook") {
+
+        } else if (authMethod === "twitter") {
+
+        }
+      });
+    }
+  });
