6e74fcab6684a29fbe091f3093c8469049918836
Justin James <digitaldrummerj@gmail.com>
2015-12-15 12:36:11 -0700

    added firebase todo with auth example

---
+++ Todo-Firebase-With-Auth/www/js/services/AuthService.js
@@ -0,0 +1,52 @@
+angular
+		.module('todoApp')
+		.factory('Auth', Auth);
+
+Auth.$inject = ['$firebaseAuth', 'FireBaseUrl', '$state'];
+function Auth($firebaseAuth, FireBaseUrl, $state) {
+  var usersRef = new Firebase(FireBaseUrl);
+  var firebaseAuth = $firebaseAuth(usersRef);
+  var service = {
+    login: login,
+    logout: logout,
+    signedIn: signedIn,
+    firebaseAuth: firebaseAuth,
+    getuser: getUser
+  };
+
+		return service;
+
+  function signedIn() {
+    return !!getUser().provider; //using !! means (0, undefined, null, etc) = false | otherwise = true
+  }
+  function logout() {
+    firebaseAuth.$unauth()
+  }
+
+  function setUser(user_data) {
+    window.localStorage["authdata"] = JSON.stringify(user_data);
+  };
+
+  function getUser() {
+    return JSON.parse(window.localStorage["authdata"] || '{}');
+  };
+
+  function login(authMethod) {
+    firebaseAuth.$authWithOAuthRedirect(authMethod).then(function (authData) {
+      console.log("logged in", authData);
+      setUser(authData);
+
+    }).catch(function (error) {
+      console.log('login error', error);
+      if (error.code === 'TRANSPORT_UNAVAILABLE') {
+        firebaseAuth.$authWithOAuthPopup(authMethod).then(function (authData) {
+          console.log("logged in", authData);
+          setUser(authData);
+        });
+      } else {
+        console.log(error);
+      }
+    })
+
+  };
+}
